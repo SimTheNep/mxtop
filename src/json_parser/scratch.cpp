9d3a70fe1ae3f117ee385c45cf181fc7d4dbd081
SimTheNep <simaovaladao05@gmail.com>
2026-07-23 15:12:00 +0000

    Initial commit

--- src/json_parser/scratch.cpp
+++
@@ -1,9 +0,0 @@
-// Delete once main.cpp has logic implemeted
-#include "parser.hpp"
-#include <fstream>
-
-int main() {
-    std::ifstream f("../../modules/gm/layouts.json");
-    nlohmann::json j; f >> j;
-    debugPrintLayouts(parseLayouts(j));
-}
